227ead3a9cbb7790ba5306a0c3f7d58281088d8f
Mikhail Polevoda <snail1704@gmail.com>
2025-07-21 15:44:09 +0400

    #1: add impl for pdf parser

--- src/stakan/core/parsers/pdf.py
+++ src/stakan/core/parsers/pdf.py
@@ -1,12 +1,37 @@
 """PDF parser, based on Docling (https://github.com/docling-project/docling)."""
 
-from docling.document_converter import DocumentConverter
+from io import BytesIO
+
+from docling.datamodel.base_models import InputFormat
+from docling.datamodel.pipeline_options import (
+    PdfPipelineOptions,
+)
+from docling.document_converter import DocumentConverter, PdfFormatOption
+from docling_core.types.io import DocumentStream
 
 
 class PDFParser:
     """Parser for PDF documents, based on Docling."""
 
+    def __init__(self) -> None:
+        pipeline_options = PdfPipelineOptions()
+        pipeline_options.do_ocr = False
+        pipeline_options.do_table_structure = True
+        pipeline_options.table_structure_options.do_cell_matching = True
+
+        self._doc_converter = DocumentConverter(
+            allowed_formats=[InputFormat.PDF],
+            format_options={
+                InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)
+            },
+        )
+
     def parse(self, document: bytes) -> str:
         """Parse document bytes into `Document`."""
-        # From docling docs: DocumentConverter().convert()
-        raise NotImplementedError
+        converter = DocumentConverter()
+
+        result = converter.convert(
+            source=DocumentStream(name="", stream=BytesIO(document)),
+        )
+
+        return result.document.export_to_markdown()
---
+++ tests/core/parsers/test_pdf.py
@@ -0,0 +1,54 @@
+"""Tests for PDF parser."""
+
+import pytest
+
+from stakan.core.parsers.pdf import PDFParser
+
+
+@pytest.fixture
+def sample_pdf_bytes() -> bytes:
+    return b"""%PDF-1.1
+1 0 obj
+<< /Type /Catalog /Pages 2 0 R >>
+endobj
+2 0 obj
+<< /Type /Pages /Kids [3 0 R] /Count 1 >>
+endobj
+3 0 obj
+<< /Type /Page /Parent 2 0 R /MediaBox [0 0 200 200]
+   /Contents 4 0 R /Resources << /Font << /F1 5 0 R >> >> >>
+endobj
+4 0 obj
+<< /Length 73 >>
+stream
+BT
+/F1 12 Tf 10 180 Td (Heading 1) Tj
+10 -20 Td (This is a simple PDF.) Tj
+ET
+endstream
+endobj
+5 0 obj
+<< /Type /Font /Subtype /Type1 /BaseFont /Helvetica >>
+endobj
+xref
+0 6
+0000000000 65535 f
+0000000009 00000 n
+0000000150 00000 n
+0000000225 00000 n
+0000000340 00000 n
+0000000460 00000 n
+trailer
+<< /Root 1 0 R /Size 6 >>
+startxref
+560
+%%EOF
+"""
+
+
+def test_parse(sample_pdf_bytes: bytes) -> None:
+    parser = PDFParser()
+
+    text = parser.parse(sample_pdf_bytes)
+
+    assert text == "## Heading 1\n\nThis is a simple PDF."
