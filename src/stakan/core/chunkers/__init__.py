e208b939da88748e8fb56314c71ce7d439b4a927
Mikhail Polevoda <snail1704@gmail.com>
2025-07-29 19:09:34 +0400

    #4: protocol for chunkers

---
+++ src/stakan/core/chunkers/__init__.py
@@ -0,0 +1,9 @@
+"""
+Chunker for split large parsed files into small chunks.
+
+Based on [5 Levels of text splitting](https://github.com/FullStackRetrieval-com/RetrievalTutorials/blob/main/tutorials/LevelsOfTextSplitting/5_Levels_Of_Text_Splitting.ipynb)
+
+---
+Links:
+    https://github.com/FullStackRetrieval-com/RetrievalTutorials/blob/main/tutorials/LevelsOfTextSplitting/5_Levels_Of_Text_Splitting.ipynb
+"""
